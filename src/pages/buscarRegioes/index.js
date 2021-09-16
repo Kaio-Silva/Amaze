683e2ea0b43d91646f9884ba3795e3a5fe46582d
Kaio-Silva <kaiosilvacoelho@gmail.com>
2021-09-16 20:29:21 -0300

    ok

---
+++ src/pages/buscarRegioes/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { Container } from './styled.js';
+
+function BuscarRegioes() {
+  return (
+    <Container>
+      <div className="Cabecalho-Reg">
+        <div className="Logo-Reg">
+          <img className="Bussola-Req" src="/assets/Images/BussolaPreta.png" alt=""/>
+          <div className="Nome-Req"> Amaze </div>
+        </div>
+        <input className="Input-Reg" type="text"/>
+      </div>
+      <div className="Conteudo-Reg">
+      <img className="Img-Req1" src="/assets/Images/Img-BuscarRegiões.png" alt=""/>
+        <div className="Textos-Req">
+          <div className="Titulo-Req"> Melhore a segurança do seu Bairro </div>
+          <div className="Descricao-Req"> Junte-se a Amaze, ajude fazendo denuncias de locais perigosos, aumentando a segurança de seu bairro </div>
+        </div>
+        <img className="Img-Req2" src="/assets/Images/Img-BuscarRegiões.png" alt=""/>
+      </div>
+    </Container>
+    
+  );
+}
+
+export default BuscarRegioes;
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import {  BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './pages';
+import BuscarRegioes from './pages/buscarRegioes';
 
 export default function Rotes() {
     return (
         <BrowserRouter>
             <Switch> 
                 <Route  path="/"  exact={true}  component={Home} />   
+                <Route  path="/Buscar" component={BuscarRegioes} />  
             </Switch>
         </BrowserRouter> 
     )
