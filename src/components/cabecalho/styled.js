36fc1c7904153bf90e0f8d37e261de845ac04a05
Kaio-Silva <kaiosilvacoelho@gmail.com>
2021-09-19 20:39:10 -0300

    ok

--- src/components/cabecalho/styled.js
+++ src/components/cabecalho/styled.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 
+
 const Container = styled.div`
 
-display:flex;
+  display:flex;
   flex-direction: column;
   font-family:  regular;  
   height:auto;
