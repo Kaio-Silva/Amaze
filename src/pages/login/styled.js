de6f433dc1fc2b19e624126f688dbc2c02b8f9d2
Kaique <sakanzel4670@gmail.com>
2021-09-21 11:56:35 -0300

    OK

--- src/pages/login/styled.js
+++ src/pages/login/styled.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
+
 const Container = styled.div`
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: center;
 
-    background-color: rgba(47, 167, 159, 0.65);
 
     height: 100vh;
 
@@ -16,7 +16,8 @@ const Container = styled.div`
        align-items: center;
        flex-direction: column;
        height: 100%;
-       width: 50%;
+       width: 33%;
+       justify-content: center;
    }
 
    .conteudo1{
@@ -34,27 +35,45 @@ const Container = styled.div`
         align-items: center;
         flex-direction: row;
         height: 50%;
-        width: 75%;
-
-        box-shadow: 0.5em 0.8em 0.5em 0.1em rgba(0, 0, 0, 0.25);
+        width: 80%;
+        
+       
+        box-shadow: 0.1em 0.10em 0.4em 0.6em rgba(0, 0, 0, 0.25);
    }
 
    .pt2{
-        background-color: white;
         display: flex;
-        align-items: center;
         flex-direction: column;
+        background-color: white;
+        align-items: center;
         height: 100%;
         width: 50%;
+        justify-content: center;
+       
    }
 
    .conteudo2{
-        background-color: white;
         display: flex;
-        align-items: center;
         flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        background-image: url('/assets/images/login_fundo.png');
+        background-size: 100% 100%;
+        background-repeat: no-repeat;
+
         height: 100%;
          width: 100%;
+   }
+ 
+   .text1{
+   
+   }
+   button {
+       color: white;
+       background-color: #24BF99;
+       height: 30px;
+       width: 200px;
    }
 
 `
