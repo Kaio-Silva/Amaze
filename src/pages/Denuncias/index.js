30ac063d20f8ee0913363616d8b57f7ae1dac71b
Thiago Viana <vianathiago171@gmail.com>
2021-09-24 00:02:45 -0300

    Ok

--- src/pages/Denuncias/index.js
+++ src/pages/Denuncias/index.js
@@ -5,18 +5,23 @@ import Cabecalho from '../../components/cabecalho';
 function Denuncias() {
     return(
         <Container>
-            <Cabecalho/>
+        <Cabecalho/>
             <div className="conteudo">
                 <div className="rua"> Rua José Bonifácio </div>
                 <div className="usuario">
                     <div className="row">
                         <div className="informações">
-                            <div className="autor A"> <b>Autor:</b> Yudji doido </div>
-                            <div className="delito A"> <b>Delito:</b> Assalto </div>
-                            <div className="avaliação"></div>
-                            <div className="descrição A"> <b>Descrição:</b> aiaiaiauiuiaiaiaiauiuiaiuiauiauiuaiauiauiujgdvhqwdvgqyuigdf qyufvbhaevfhQWEFVQWYeurfwyqegfwyegfuasegfuiasgfhsabvflshiILGBIYovdfilydv </div>
+                            <div className="autor A"> <b>Autor: </b> Yudji doido </div>
+                            <div className="delito A"> <b>Delito: </b> Assalto </div>
+                            <div className="avaliação A"> <img src="/assets/Images/aval.png"/> </div>
+                            <div className="descrição A">
+                                <b>Descrição: </b>
+                                <div className="descriçao">
+                                    aiaiaiauiuiaiaiaiauiuiaiuiauiauiuaiauiauiujgdvhqwdvgqyuigdf qyufvbhaevfhQWEFVQWYeurfwyqegfwyegfuasegfuiasgfhsabvflshiILGBIYovdfilydv
+                                </div>
+                            </div>
                         </div>
-                        <div className="Data"><div className="data"> <b>Data:</b> 20/09/2021 </div></div>
+                        <div className="Data"><b>Data:</b> 20/09/2021 </div>
                     </div>
                     <div className="botões">
                         <div className="botão1"> <button> Avaliar Denuncia </button> </div>
--- src/pages/Denuncias/styled.js
+++ src/pages/Denuncias/styled.js
@@ -2,6 +2,11 @@ import styled from 'styled-components'
 
 
 const Container = styled.div`
+
+    .conteudo {
+        box-shadow: 2em;
+    }
+
     .usuario {
         background-color: #24BF99;
         /*height: 88.4vh;*/
@@ -12,27 +17,72 @@ const Container = styled.div`
         flex-direction: column;
         align-items: center;
         font-size: 2em;
+        border-top: solid;
+        padding: 0.4em 0em;
+        font-family: medio
     }
 
     .row {
         display: flex;
         flex-direction: row;
-        padding-left: 5em;
         color: white;
         font-size: 1.3em;
+        font-family: regular;
     }
 
-    .A {padding-top: 2em;}
+    .informações {
+        padding-left: 7em;
+    }
+
+    .autor{
+        padding-top: 3em;
+    }
+
+    .A {
+        padding-top: 1em;
+    }
+
+    img {
+        width: auto;
+        height: 1em;
+    }
 
     .descrição{
         display: flex;
-        flex-direcion: column;
+        flex-direction: column;
     }
 
-    b {padding-bottom: 5em;}
+    descriçao {
+        font-size: 2em;
+    }
+
+    .Data{
+        display: flex;
+        flex-direction: row;
+        padding-right: 8em;
+    }
+
+    b{
+        font-size: 1.1em;
+        padding-right: 0.2em;
+    }
+
+    .botões {
+        display: flex;
+        flex-direction: row;
+        padding: 3em 0em 5em 42em;
+    }
+
+    .botão1 {
+        padding-right: 1em;
+    }
 
     button {
-        padding: 1em 2.5em;;
+        padding: 0.5em 1.5em;
+        border: none;
+        border-radius: 0.5em;
+        font-size: 1.3em;
+        background-color: white;
     }
 `
 
