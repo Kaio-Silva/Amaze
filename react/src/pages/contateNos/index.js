e692dfe8c477f27844f8bd39cb63aa523ed17c5a
Beatriz <beatrix134santos@gmail.com>
2021-09-30 21:19:52 -0300

    ok

--- react/src/pages/contateNos/index.js
+++ react/src/pages/contateNos/index.js
@@ -1,8 +1,33 @@
-import {Container} from './styled'
-export default function contact(){
+import { Container } from './styled'
+import { Link } from 'react-router-dom';
+import Logo from '../../components/logo'
+import { Padrao } from '../../components/inputs/styled';
+import { Button } from '../../components/botoes/styled';
+
+export default function Contatnos(props){
     return(
         <Container>
-           <h1>ola</h1>
+            <Logo ambos="false"  cor="white" titulo="" tamanho="medio"/>
+            <div className="BoxLogin">
+                <div className="ConteudoAmaze">
+                    <Logo direction="column" cor="white" titulo="block" tamanho="grande"/>
+                    <div className="DescLogin">Faça sua segurança com a Amaze, se torne um de nos</div>
+                </div>
+                <div className="ConteudoLogin">
+                    <div className="TituloLogin"> Contate-nos </div>
+                    <div className="FormLogin">
+                        <div className="Inputs">                        
+                            <div className="Titulo">Email</div>
+                            <Padrao placeholder="Insira seu email"/>
+                        </div>
+                        <div className="Inputs">                        
+                            <div className="Titulo">Senha</div>
+                            <Padrao className="senha" placeholder="Insira seu login" type="password" Type="senha"/>
+                        </div>
+                        <Link  to="/Relatorio"><Button className="ajustarBotao" tamanho="medio" cor="0FA882"> Enviar </Button></Link>
+                    </div>
+                </div>
+            </div>
         </Container>
     )
 }
