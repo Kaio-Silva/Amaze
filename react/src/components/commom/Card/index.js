20f47db2bd83308dd4c10383982db4073503abeb
KaiqueYudji <yudjikaique@gmail.com>
2021-10-03 16:46:01 -0300

    ok

--- react/src/components/commom/Card/index.js
+++ react/src/components/commom/Card/index.js
@@ -1,8 +1,12 @@
 import { Card } from './styled';
+import Pop from "../../../Popups/popAdm"
+import { useState } from 'react';
 
-export default function denuncias(props) {
+export default function Denuncias(props) {
+    const[trigger,SetTrigger] = useState(false)
     return (
         <Card>
+            <div onClick={() => SetTrigger(true)}>
                 <div className="cont i"> <div className="b">Autor:</div> {props.autor} </div>
                 <div className="cont i"> <div className="b">Qtd Reportes:</div> {props.qtd} </div>
                 <div className="descriçao i"> <div className="b">Descrição:</div> {props.desc} </div>
@@ -10,6 +14,8 @@ export default function denuncias(props) {
                     <button className="botoes"> <img src="/assets/Images/CancelarDenuncia3.png" alt=""/> </button>
                     <button className="botoes"> <img src="/assets/Images/Aprovar3.png" alt=""/> </button>
                 </div>
+             </div>
+                <Pop trigger={trigger} SetTrigger={SetTrigger}></Pop>
         </Card>
     )
 }
---
+++ react/src/components/commom/CardPop/index.js
@@ -0,0 +1,12 @@
+import { Container } from './styled'
+
+export default function Card(){
+    return(
+      <Container>
+          <div className="nm">Nome: <div>Kaique</div> </div>
+          <div className="motivo">Motivo:<div>Assalto</div> </div>
+      </Container>
+    )
+}
+
+
