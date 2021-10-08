e947944943ab189a2a83ba76c404744fa8760bdf
KaiqueYudji <yudjikaique@gmail.com>
2021-10-08 18:53:52 -0300

    ok

--- react/src/Popups/menu/styled.js
+++ react/src/Popups/menu/styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 const Container = styled.div`
 display: flex;
-justify-content: left;
+justify-content: right;
 align-items: top;
 background-color: rgba(0,0,0,0.2);
 height: 100vh;
@@ -155,6 +155,10 @@ padding-bottom:0.5em ;
 font-size: 1em;
 color: ${props => props.background === 'white' ? 'black'
                                                              :'white'};     
+}
+
+@media(max-width:1024px){
+    width: 100%;
 }
 
 `
