9db70be7df9d56ec8b183d8c823a76ecae2ca91e
KaiqueYudji <yudjikaique@gmail.com>
2021-10-08 18:32:35 -0300

    ok

--- react/src/Popups/menu/styled.js
+++ react/src/Popups/menu/styled.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 const Container = styled.div`
 display: flex;
 justify-content: right;
-align-items: center;
+align-items: top;
 background-color: rgba(0,0,0,0.2);
 height: 100vh;
 width: 100%;
