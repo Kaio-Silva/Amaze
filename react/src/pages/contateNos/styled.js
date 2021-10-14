7caa8bde7b72f4300003640b3e1aecfad772540d
KaiqueYudji <yudjikaique@gmail.com>
2021-10-14 16:42:03 -0300

    ok

--- react/src/pages/contateNos/styled.js
+++ react/src/pages/contateNos/styled.js
@@ -125,8 +125,7 @@ const Container = styled.div`
    }
 
     .input{
-        width: 22em;
-        margin-right:1em;
+        width:49.2%;
     }
 
     .tamanho{
@@ -228,6 +227,13 @@ const Container = styled.div`
         }
 
    }
+
+   @media(min-width:1400px){
+     .Inputs{
+         height:21em;
+     }
+   }
+
 `
 
 export { Container }
