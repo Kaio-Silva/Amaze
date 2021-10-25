304f222b73de71d74f3ee25d6647fc8ab29c5de8
MarcosHTMLE <marcosprogramador123@gmail.com>
2021-10-25 11:34:25 -0300

    ok

--- react/src/pages/cadastrar/index.js
+++ react/src/pages/cadastrar/index.js
@@ -7,8 +7,25 @@ import { Link } from 'react-router-dom';
 import { Padrao } from '../../components/styled/inputs.js';
 
 import { Container } from './styled.js';
+import { useState } from 'react';
+import Api from '../../services/api.js';
+
+const api = new Api()
+
+
 
 function Cadastrar(props) {
+   const [nome,setNome] = useState('')
+   const [email,setEmail] = useState('')
+   const [telefone,setTelefone] = useState('')
+   const [regiao,setRegiao] = useState('')
+   const [senha,setSenha] = useState('')
+   const [concluir,setConcluir] = useState([])
+
+   async function Inserir(){
+      let a = await api.InserirUSU(nome, email, telefone, regiao, senha, concluir)
+   }
+
   return (
     <Container>
           <div className="ajustarLogo"><Logo cor="black" tamanho="grande"/></div>
@@ -17,31 +34,32 @@ function Cadastrar(props) {
                  <div className="TituloCadastro"> Cadastro </div>
                  <div className="Inputs">                        
                     <div className="Titulo">Nome Completo</div>
-                    <Padrao className="padrao" placeholder="Insira seu nome"  tamanho="100%" />
+                    <Padrao onChange={ e => setNome(e.target.value)} className="padrao" placeholder="Insira seu nome"  tamanho="100%" />
                  </div>
                  <div className="Inputs">                        
                     <div className="Titulo">Email</div>
-                    <Padrao className="padrao"  placeholder="Insira seu email" tamanho="100%" />
+                    <Padrao onChange={ e => setEmail(e.target.value)} className="padrao"  placeholder="Insira seu email" tamanho="100%" />
                  </div>
                  <div className="Inputs">                        
                     <div className="Titulo">Telefone</div>
-                    <Padrao  className="padrao" placeholder="Insira seu telefone" tamanho="100%" />
+                    <Padrao onChange={ e => setTelefone(e.target.value)} className="padrao" placeholder="Insira seu telefone" tamanho="100%" />
                  </div>
                  <div className="Inputs">                        
                     <div className="Titulo">Região</div>
-                    <Padrao className="padrao"  placeholder="Insira sua região" tamanho="100%" />
+                    <Padrao onChange={ e => setRegiao(e.target.value)} className="padrao"  placeholder="Insira sua região" tamanho="100%" />
                  </div>
                  <div className="Inputs">                        
                     <div className="Titulo">Senha</div>
-                    <Padrao className="padrao"  placeholder="Insira seu senha" tamanho="100%" />
+                    <Padrao onChange={ e => setSenha(e.target.value)} className="padrao"  placeholder="Insira seu senha" tamanho="100%" />
                  </div>          
                   <div className="mae-botao">
-                      <Link className="link" to="/Login"><Button className="ajustarBotao" cor=""> Concluir Cadastro </Button></Link>
+                      <Link className="link" to="/Login"><Button className="ajustarBotao" onClick={Inserir}  cor=""> Concluir Cadastro </Button></Link>
                   </div>
               </div>
             </div>
     </Container>
   );
 }
+
 
 export default Cadastrar;
