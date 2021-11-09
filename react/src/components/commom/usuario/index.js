ddc994e35916ea0677a556558cee2b67983cd707
flygon525 <vianathiago171@gmail.com>
2021-11-09 11:31:38 -0300

    ok

--- react/src/components/commom/usuario/index.js
+++ react/src/components/commom/usuario/index.js
@@ -4,31 +4,30 @@ import { Container } from './styled.js'
 import Api from "../../../services/api";
 const api = new Api()
 
-export default function Usuario(){
+export default function Usuario(props){
     const[usuario, setUsu] = useState([]);
 
-    async function fUsuario(){
+    async function Usuario(){
         let f = await api.USUtotal();
         console.log(f)
     }
 
     useEffect(() =>{
-        fUsuario();
+        Usuario();
     }, [])
 
     return(
-
         <Container>
             <div className="alinhandoInformacoes">
                 <div className="informacoes">
-                    <div className="texto"> <span> Nome: </span> Thiago Viana Menezes </div>
-                    <div className="texto"> <span> Data: </span> 10/03/2005 </div>
-                    <div className="texto"> <span> Telefone: </span> (11) 95845-1086 </div>
+                    <div className="texto"> <span> Nome: </span> {props.Nome} </div>
+                    <div className="texto"> <span> Data: </span> {props.Data} </div>
+                    <div className="texto"> <span> Telefone: </span> {props.Telefone} </div>
                 </div>
                 <div className="informacoes">
-                    <div className="texto"> <span> Cadastro: </span> 14/08/2020 </div>
-                    <div className="texto"> <span> Email: </span> vianathiago171@gmail.com </div>
-                    <div className="texto"> <span> Reportes: </span> 8 </div>
+                    <div className="texto"> <span> Cadastro: </span> {props.Cadastro} </div>
+                    <div className="texto"> <span> Email: </span> {props.Email} </div>
+                    <div className="texto"> <span> Reportes: </span> {props.Report} </div>
                 </div>
                 <div className="alinhandoImg">
                     <img  className="imagem" src="/assets/Images/CancelarDenuncia3.png" alt=""/> 
--- react/src/pages/adm/removerUsuario/index.js
+++ react/src/pages/adm/removerUsuario/index.js
@@ -1,19 +1,35 @@
 import Usuario from "../../../components/commom/usuario";
 import { Pesquisa } from "../../../components/styled/inputs.js";
 import { Container } from "./styled";
+import { useEffect, useState } from "react";
+import Api from "../../../services/api";
+
+export default function Pagina(props) {
+    const [usu, setUsu] = useState([]);
+
+    async function ListarUsuario(){
+        let f = await Api.USUtotal();
+
+        setUsu(f);
+    }
+
+    useEffect(() => {
+        ListarUsuario();
+    }, [])
 
 
-export default function pagina() {
     return (
             <Container>
                 <Pesquisa className="Pesquisar" input="usar" tamanho="medio"/>
-                <div className="baixo">
-                   <Usuario />
-                   <Usuario />
-                   <Usuario />
-                   <Usuario />
-                   <Usuario />
-                </div>
+                {usu.map(()=>
+                    <div className="baixo">
+                    <Usuario Nome="Thiago Viana" Data="10/03/2005" Telefone="4002 8922" Cadastro="09/11/2021" Email="asasasasa@gmail.com" Report="8"/>
+                    <Usuario />
+                    <Usuario />
+                    <Usuario />
+                    <Usuario />
+                    </div>
+                )}
             </Container>
     )
 }
