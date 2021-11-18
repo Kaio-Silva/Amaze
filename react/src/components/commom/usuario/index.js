67d7c6602c84030c781ba73526c09a4625c2e151
flygon525 <vianathiago171@gmail.com>
2021-11-18 11:58:29 -0300

    ok

--- react/src/components/commom/usuario/index.js
+++ react/src/components/commom/usuario/index.js
@@ -1,24 +1,28 @@
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Container } from './styled.js'
 import Api from "../../../services/api";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import LoadingBar from 'react-top-loading-bar';
 const api = new Api()
 
 export default function Usuario(props){
+    const loading = useRef(null);
 
     async function DeletUsu(){
-        console.log('oi')
+        loading.current.continuosStart();
         let f = await api.DELETEusu(props.id)
         toast.success('aiaiaia')
         props.func()
+        loading.current.complete();
         
     }
 
     return(
         <Container>
             <ToastContainer/>
+            <LoadingBar ref={loading}/>
             <div className="alinhandoInformacoes">
                 <div className="informacoes">
                     <div className="texto"> <span> Nome: </span> {props.Nome} </div>
